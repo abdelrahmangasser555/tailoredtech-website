a486a910746f85dade3d4d09bc77aca676ca9477
abdelrahmangasser555 <abdelrahmangasser2023@gmail.com>
2024-10-18 19:29:11 +0300

    changed the image

--- src/components/general/home/standartCompare.tsx
+++ src/components/general/home/standartCompare.tsx
@@ -5,8 +5,8 @@ export function CompareHome() {
   return (
     <div className="p-4 border rounded-3xl dark:bg-neutral-900  dark:border-neutral-800 px-4">
       <Compare
-        firstImage="https://deistor4v34pj.cloudfront.net/2024-10-05T12-04-02.367Z-Screenshot%202024-10-01%20182645.png"
-        secondImage="https://deistor4v34pj.cloudfront.net/2024-10-05T12-06-40.608Z-Screenshot%202024-10-05%20150601.png"
+        firstImage="https://th.bing.com/th/id/R.15b44d88143c4f15e63eeeaaa9856bab?rik=MRu%2fx0hrSz92Kg&pid=ImgRaw&r=0"
+        secondImage="https://th.bing.com/th/id/OIP.KB1miCPp2j4gNiolUYZlKwHaE8?rs=1&pid=ImgDetMain"
         firstImageClassName="object-cover object-left-top"
         secondImageClassname="object-cover object-left-top"
         className="h-[250px] w-[200px] md:h-[500px] md:w-[500px]"
